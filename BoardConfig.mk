fd92f23c3188f5f906a18bed08d91821f0d03e42
Michael J. Cusack <WhoNeedszZz@gmail.com>
2015-05-23 21:34:56 -0500

    Add missing KERNEL_IMAGE_NAME & Set permissive sepolicy
    Change-Id: I277eabef327583dc62b76f71590cb3526a782a2c
    Signed-off-by: Michael J. Cusack <WhoNeedszZz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,12 +34,14 @@ TARGET_NO_BOOTLOADER := true
 TARGET_KERNEL_CONFIG := vindicator_defconfig
 TARGET_KERNEL_SOURCE := kernel/moto/shamu
 
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M androidboot.selinux=permissive
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset BOARD_RAMDISK_OFFSET --tags_offset BOARD_KERNEL_TAGS_OFFSET
 
